447b4bce6e76bbadc39b2aa69ddb195317f480be
amie-affection <amie.affection@gmail.com>
2020-06-21 17:44:34 +0300

    modified files

--- src/components/statistic/Statistic.js
+++ src/components/statistic/Statistic.js
@@ -1,22 +1,24 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Statistic extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
-
-  render() {
-    return (
-      <>
-        <h2>Statistic:</h2>
-        <p>Good: <span>{this.state.good}</span></p>
-        <p>Neutral: <span>{this.state.neutral}</span></p>
-        <p>Bad: <span>{this.state.bad}</span></p>
-      </>
-    );
-  }
-}
+const Statistic = ({ good, neutral, bad, total, positivePercentage }) => {
+  // console.log(total);
+  // console.log(positivePercentage);
+  return (
+    <>
+      <h2>Statistic:</h2>
+      {total > 0 ? (
+        <>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>Total: {total}</p>
+          <p>Positive feedback: {positivePercentage}%</p>
+        </>
+      ) : (
+        <p>No feedback given</p>
+      )}
+    </>
+  );
+};
 
 export default Statistic;
